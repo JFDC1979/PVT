f5d8ee27a294452db4f052f8ddc369488cfff9f8
MDS <MDS@DESKTOP-8O52F8U>
2025-04-21 20:21:49 -0300

    Agregar archivos de proyecto.

---
+++ PVTv1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PVTv1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
